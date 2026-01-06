b9ce2425faed0566a0dc58fd84e45db4534194f0
6811130005MUT <6811130005@mut.ac.th>
2026-01-06 15:43:29 +0700

    LAB8

---
+++ งานปี1เทอม2/LAB8/LAB8_1/LAB8_1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+    string id;
+    string nickname;
+    string lineId;
+    string phone;
+};
+
+int main() {
+    Student s1;
+
+    cout << "=== Input Student 1 ===" << endl;
+    cout << "Enter ID: ";
+    cin >> s1.id;
+
+    cout << "Enter Nickname: ";
+    cin >> s1.nickname;
+
+    cout << "Enter LineID: ";
+    cin >> s1.lineId;
+
+    cout << "Enter Phone: ";
+    cin >> s1.phone;
+
+    cout << "\n=== Output Student 1 ===" << endl;
+    cout << "ID: " << s1.id << endl;
+    cout << "Nickname: " << s1.nickname << endl;
+    cout << "LineID: " << s1.lineId << endl;
+    cout << "Phone: " << s1.phone << endl;
+
+    return 0;
+}
---
+++ งานปี1เทอม2/LAB8/LAB8_7/LAB8_7.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct Ph {
+    string mob;
+    string hom;
+};
+
+struct Par {
+    string nm;
+    string rel;
+    Ph con;
+};
+
+struct Stu {
+    string id;
+    string nick;
+    string line;
+    Ph myCon;
+    Par myPar;
+};
+
+void input(Stu& s) {
+    cout << "Input ID: ";
+    cin >> s.id;
+    cout << "Input Nickname: ";
+    cin >> s.nick;
+    cout << "Input Line: ";
+    cin >> s.line;
+
+    cout << "Input Stu Mobile: ";
+    cin >> s.myCon.mob;
+    cout << "Input Stu Home: ";
+    cin >> s.myCon.hom;
+
+    cout << "Input Parent Name: ";
+    cin >> s.myPar.nm;
+    cout << "Input Relation: ";
+    cin >> s.myPar.rel;
+
+    cout << "Input Parent Mobile: ";
+    cin >> s.myPar.con.mob;
+    cout << "Input Parent Home: ";
+    cin >> s.myPar.con.hom;
+}
+
+void show(Stu s) {
+    cout << "\n--- Student ---" << endl;
+    cout << "ID: " << s.id << endl;
+    cout << "Nick: " << s.nick << endl;
+    cout << "Line: " << s.line << endl;
+
+    cout << "Mobile: " << s.myCon.mob << endl;
+    cout << "Home: " << s.myCon.hom << endl;
+
+    cout << "\n--- Parent ---" << endl;
+    cout << "Name: " << s.myPar.nm << endl;
+    cout << "Rel: " << s.myPar.rel << endl;
+    cout << "Mobile: " << s.myPar.con.mob << endl;
+    cout << "Home: " << s.myPar.con.hom << endl;
+}
+
+int main() {
+    Stu s;
+    input(s);
+    show(s);
+    return 0;
+}
